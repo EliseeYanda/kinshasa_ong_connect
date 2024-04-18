0849d3dc10e4bd3fb0ad545c3e5a25da3fe79886
EliseeYanda <eliseeyanda05@gmail.com>
2024-04-18 11:16:21 +0100

    styliser pahe home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -20,27 +20,29 @@ const Home = () => {
       </div>
     </div>
 
-    <div className="flex bg-gray-100 p-5  p-5 m-5 space-x-80">
-      {/* Section À PROPOS */}
-      <div className="text-black ">
-        <h1 className="text-3xl font-bold p-5 text-[#AF631D] ">BIENVENU(e)</h1>
-        <p className="text-xl font-bold p-5">KINSHASA ONG connect, meilleur plateforme.</p>
-        
-      </div>
-      <div className=''>
-        <p className="mt-20 p-5 m-5">
-          Kinshasa ONG Connect est le carrefour où les organisations non gouvernementales 
+    <div className=" bg-gray-200 bg-cover bg-center p-8 m-5">
+      <div className="flex p-5 m-5">
+        <div className="md:w-1/2">
+          {/* Remplacez '/path/to/video-thumbnail.jpg' par le chemin réel de votre vignette vidéo ou image */}
+          <h1 className='text-5xl font-bold p-5 text-[#AF631D]'>BIENVENU(e)</h1>
+          <h3 className='text-2xl font-bold p-5'>KINSHASA ONG Connect, meilleur plateforme. </h3>
+        </div>
+        <div className="md:w-1/2 flex flex-col justify-center p-4">
+          <h1 className="text-3xl font-bold mb-3">Ensemble pour Kinshasa, unis pour l’humanité</h1>
+          <p className="mb-3">Kinshasa ONG Connect est le carrefour où les organisations non gouvernementales 
           de Kinshasa se rencontrent pour tisser un réseau de solidarité et d'innovation. 
           Notre plateforme est dédiée à la collaboration, au partage des ressources et à l'amplification 
-          de l'impact social à travers la capitale congolaise.
-        </p>
+          de l'impact social à travers la capitale congolaise..</p>
+          
+        </div>
       </div>
     </div>
 
     <div className="container mx-auto p-6">
-      <h1 className="text-4xl font-bold text-center mb-6">Qui nous sommes</h1>
+      <h1 className="text-4xl font-bold text-center mb-6">Qui nous sommes ?</h1>
       <p className="text-lg text-center mb-6">
-        Organisation créée pour fournir de l'aide et collecter des fonds à ceux qui en ont besoin.
+        KINSHASA ONG Connect créée pour présenter les défis et collecter des fonds 
+        pour les organisations non gouvernementales.
       </p>
       <div className="grid md:grid-cols-3 gap-4">
         <div className="bg-white p-4 rounded-lg shadow-md">
@@ -69,9 +71,11 @@ const Home = () => {
         </div>
         <div className="md:w-1/2 flex flex-col justify-center p-4">
           <h1 className="text-3xl font-bold mb-3">Communauté de collecte de fonds</h1>
-          <p className="mb-3">Nulla fermentum turpis id nunc tincidunt, vitae hendrerit orci viverra. Nulla facilisi. Duis tempor nisi nec dolor malesuada viverra. Morbi id tempus nisi, à divers enim. Morbi eget augue neque, in iugula.</p>
+          <p className="mb-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+          Autem iure repellendus quidem? Sint laborum minus laudantium fuga labore nesciunt amet autem impedit cum, 
+          fugit vel totam ipsa, ab consequatur. Laboriosam?.</p>
           <button className="bg-[#AF631D] text-white font-bold py-2 px-4 rounded transition duration-300">
-            En savoir plus
+            Faire un don
           </button>
         </div>
       </div>
@@ -82,8 +86,24 @@ const Home = () => {
         <h2 className="text-4xl font-bold text-white ">Ensemble nous pouvons</h2>
         <p className="text-white mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
         <button className="bg-[#AF631D] text-white  py-2 px-4 rounded">
-          Faire un don
+          Contactez-Nous
         </button>
+      </div>
+    </div>
+
+    <div className="bg-gray-100 p-10 flex flex-col items-center justify-center">
+      <div className="bg-white shadow-lg rounded-full w-24 h-24 mb-4">
+        {/* Insérez ici l'avatar de la personne */}
+      </div>
+      <blockquote className="italic text-center">
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Impedit ipsam fuga sunt qui excepturi optio porro magni tempore libero voluptatum illo iste, 
+        similique officia commodi perferendis voluptate culpa odio recusandae!."
+      </blockquote>
+      <div className="flex mt-4">
+        <span className="h-3 w-3 bg-gold-500 rounded-full mr-1"></span>
+        <span className="h-3 w-3 bg-orange-500 rounded-full mr-1"></span>
+        <span className="h-3 w-3 bg-black rounded-full"></span>
       </div>
     </div>
 
